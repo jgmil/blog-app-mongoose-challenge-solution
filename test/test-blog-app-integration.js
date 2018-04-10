f142580ae15ecd3bfee6b1a0b079b422b8b69841
Julia Miller <opie.julia@gmail.com>
2018-04-09 23:40:51 -0400

    updated GET tests

--- test/test-blog-app-integration.js
+++ test/test-blog-app-integration.js
@@ -20,12 +20,13 @@ const {
 chai.use(chaiHttp);
 
 function seedBlogPostData() {
-    console.info('seeding restaurant data');
+    //console.info('seeding restaurant data');
     const seedData = [];
 
-    for (let i = 1; i <= 10, i++) {
+    for (let i = 1; i <= 10; i++) {
         seedData.push(generateBlogPostData());
     }
+    //console.info(seedData);
     return BlogPost.insertMany(seedData);
 }
 
@@ -33,8 +34,8 @@ function generateBlogPostData() {
     return {
         title: faker.name.title(),
         author: {
-            firstName: faker.name.firstName,
-            lastName: fake.name.lastName,
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
         },
         content: faker.lorem.paragraph(),
     };
@@ -54,7 +55,7 @@ describe('Blog Post API resource', function () {
         return seedBlogPostData();
     });
 
-    afterEach(fucntion() {
+    afterEach(function () {
         return tearDownDb();
     });
 
@@ -62,4 +63,49 @@ describe('Blog Post API resource', function () {
         return closeServer();
     });
 
+    describe('GET endpoint', function () {
+        it('should return all existing blog posts', function () {
+            let res;
+            return chai.request(app)
+                .get('/posts')
+                .then(function (_res) {
+                    res = _res;
+                    //console.log(res.body);
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.length.of.at.least(1);
+                    return BlogPost.count();
+                })
+                .then(function (count) {
+                    expect(res.body).to.have.length.of(count);
+                });
+        });
+        it('should return blog posts with the right fields', function () {
+            let resBlogPost;
+            return chai.request(app)
+                .get('/posts')
+                .then(function (res) {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    expect(res.body).to.be.a('array');
+                    expect(res.body).to.have.length.of.at.least(1);
+
+                    res.body.forEach(function (blogpost) {
+                        console.log(blogpost);
+                        expect(blogpost).to.be.a('object');
+                        expect(blogpost).to.include.keys(
+                            'id', 'title', 'author', 'content', 'created');
+                    });
+                    resBlogPost = res.body[0];
+                    return BlogPost.findById(resBlogPost.id);
+                })
+                .then(function (blogpost) {
+                    expect(resBlogPost.id).to.equal(blogpost.id);
+                    expect(resBlogPost.title).to.equal(blogpost.title);
+                    expect(resBlogPost.author).to.equal(blogpost.author);
+                    expect(resBlogPost.content).to.equal(blogpost.content);
+                    expect(resBlogPost.created).to.equal(blogpost.created);
+
+                });
+        });
+    });
 });
